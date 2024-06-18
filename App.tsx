9780e36b61f64d020e8a9b6bffdecd468e134a40
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-06-18 19:58:18 +0900

    ✅ [add] conversation list 컴포넌트 추가하여 코드 분리
    navigation에 필요한 패키지 다운로드

--- App.tsx
+++ App.tsx
@@ -1,74 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    TextInput,
-    Alert,
-    ScrollView,
-    Image,
-} from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
-import { theme } from './colors';
-import { getConversationListFromServer } from 'api/conversationApi';
-import { Conversation } from 'utils/type';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ConversationList from 'page/ConversationList';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: theme.bg,
-        paddingHorizontal: 20,
-    },
-    image: {
-        width: 30,
-        height: 30,
-        borderRadius: 5,
-    },
-    titleBox: {
-        flexDirection: 'row',
-    },
-});
+const Stack = createStackNavigator();
 
 export default function App() {
-    const [conversationList, setConversationList] = useState<Conversation[]>([]);
-
-    useEffect(() => {
-        getConversationListFromServer().then((conversations) => {
-            console.log(conversations);
-            if (conversations) setConversationList(conversations);
-        });
-    }, []);
-
     return (
-        <ScrollView style={styles.container}>
-            {conversationList.map((conversation) => (
-                <TouchableOpacity
-                    key={conversation.id}
-                    style={{
-                        backgroundColor: theme.grey,
-                        paddingVertical: 10,
-                        paddingHorizontal: 20,
-                        borderRadius: 5,
-                        marginBottom: 10,
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                    }}
-                >
-                    <View style={styles.titleBox}>
-                        <View>
-                            <Image
-                                style={styles.image}
-                                source={{ uri: conversation.userAvatarUrl }}
-                            ></Image>
-                        </View>
-                        <View>
-                            <Text>{conversation.title}</Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-            ))}
-        </ScrollView>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={ConversationList} />
+                {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
---
+++ src/page/ConversationList.tsx
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput,
+    Alert,
+    ScrollView,
+    Image,
+} from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
+import { theme } from 'utils/colors';
+import { getConversationListFromServer } from 'api/conversationApi';
+import { Conversation } from 'utils/type';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        paddingHorizontal: 20,
+    },
+    image: {
+        width: 30,
+        height: 30,
+        borderRadius: 5,
+    },
+    titleBox: {
+        flexDirection: 'row',
+    },
+});
+
+export default function ConversationList() {
+    const [conversationList, setConversationList] = useState<Conversation[]>([]);
+
+    useEffect(() => {
+        getConversationListFromServer().then((conversations) => {
+            console.log(conversations);
+            if (conversations) setConversationList(conversations);
+        });
+    }, []);
+
+    return (
+        <ScrollView style={styles.container}>
+            {conversationList.map((conversation) => (
+                <TouchableOpacity
+                    key={conversation.id}
+                    style={{
+                        backgroundColor: theme.grey,
+                        paddingVertical: 10,
+                        paddingHorizontal: 20,
+                        borderRadius: 5,
+                        marginBottom: 10,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <View style={styles.titleBox}>
+                        <View>
+                            <Image
+                                style={styles.image}
+                                source={{ uri: conversation.userAvatarUrl }}
+                            ></Image>
+                        </View>
+                        <View>
+                            <Text>{conversation.title}</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            ))}
+        </ScrollView>
+    );
+}
