bd697df46977322847e4bd7828805f5194fb0ee2
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-06-19 21:27:23 +0900

    🐛 [fix] web 프로젝트에서 socket CORS가 발생하여 통신이 안 되던 오류

--- server.js
+++ server.js
@@ -5,8 +5,26 @@ const socketIO = require('socket.io');
 const { v4 } = require('uuid');
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const io = socketIO(server, {
+    cors: {
+        origin: ['http://192.168.0.7:19006', 'http://192.168.0.8:19006'],
+        methods: ['GET', 'POST'],
+        credentials: true,
+    },
+});
 const LoremIpsum = require('lorem-ipsum').LoremIpsum;
+
+// CORS 설정 (일반 HTTP 요청)
+app.use((req, res, next) => {
+    const allowedOrigins = ['http://192.168.0.7:19006', 'http://192.168.0.8:19006'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // 소켓 연결 이벤트 처리
 io.on('connection', (socket) => {
