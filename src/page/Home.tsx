d69eac51146a2aa064373305e76c80601fba75b9
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-06-28 16:36:19 +0900

    ✨ [feat] 버튼 누르면 대화목록 페이지로 이동
    resolve #12

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -3,15 +3,18 @@ import { Text, View, TouchableOpacity, ScrollView, Alert, Button } from 'react-n
 import style from '../style/Home';
 import { TextInput } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 const userIds = ['minim12', 'magna12', 'irure12', 'amet12', 'velit12'];
 
 export default function Home() {
+    const navigation = useNavigation();
     const [myId, setMyId] = useState<string>('velit12');
 
     const handlePressEnterBtn = useCallback(() => {
         if (userIds.includes(myId)) {
             AsyncStorage.setItem('myId', myId);
+            navigation.navigate('ConversationList');
             return;
         }
         Alert.alert('invalid id');
