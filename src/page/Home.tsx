ab6a595f1d48426cb916a4b1c9cc4fd825e6336c
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-06-28 16:33:56 +0900

    ✨ [feat] 만약 없는 아이디라면 다시 입력하도록
    doing #12

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,14 +1,20 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Text, View, TouchableOpacity, ScrollView, Image, Button } from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView, Alert, Button } from 'react-native';
 import style from '../style/Home';
 import { TextInput } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const userIds = ['minim12', 'magna12', 'irure12', 'amet12', 'velit12'];
+
 export default function Home() {
-    const [myId, setMyId] = useState<string>('');
+    const [myId, setMyId] = useState<string>('velit12');
 
     const handlePressEnterBtn = useCallback(() => {
-        AsyncStorage.setItem('myId', myId);
+        if (userIds.includes(myId)) {
+            AsyncStorage.setItem('myId', myId);
+            return;
+        }
+        Alert.alert('invalid id');
     }, [myId]);
 
     return (
