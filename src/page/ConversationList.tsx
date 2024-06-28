0aea3d99f5c8be5036b7d8f5b99b14a47d8112e8
keinn51 <79993356+keinn51@users.noreply.github.com>
2024-06-28 16:45:14 +0900

    ✨ [feat] Async Storage에서 아이템 가져오기 & userId를 제외한 대화목록을 보여주기
    resolve #13

--- src/page/ConversationList.tsx
+++ src/page/ConversationList.tsx
@@ -1,19 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { getConversationListFromServer } from '../api/conversationApi';
 import { ConversationType } from '../utils/type';
 import { useNavigation } from '@react-navigation/native';
 import moment from 'moment';
 import style from '../style/ConversationList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function ConversationList() {
     const navigation = useNavigation();
     const [conversationList, setConversationList] = useState<ConversationType[]>([]);
 
+    const initComponent = useCallback(() => {
+        AsyncStorage.getItem('myId').then((myId) => {
+            getConversationListFromServer().then((conversations) => {
+                const newConversationList = conversations.filter(
+                    (conversation: ConversationType) => {
+                        return conversation.userId !== myId;
+                    },
+                );
+                setConversationList(newConversationList);
+            });
+        });
+    }, []);
+
     useEffect(() => {
-        getConversationListFromServer().then((conversations) =>
-            setConversationList(conversations || []),
-        );
+        initComponent();
     }, []);
 
     return (
